1e997648a05cf4d95158c3b5c46037bf5be94212
WatchDG <baranov@w3m.co>
2021-05-29 09:01:43 +0300

    update deps

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ yarn install http-instance-request
 # or
 npm install http-instance-request
 ```
+
+## example
+
+```ts
+import {request} from "http-instance-request";
+import {URL} from "url";
+
+(async () => {
+
+    const {status, headers, body} = (await request({
+        url: new URL('https://example.com'),
+        options: {
+            method: 'GET'
+        }
+    })).unwrap();
+
+    console.log('> status:', status);
+    console.log('> headers:', headers);
+    console.log('> body:', body);
+
+})();
+```
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,55 @@
-import * as http from 'http';
-import * as https from 'https';
-import {URL} from 'url';
-import type {IncomingHttpHeaders} from "http";
-import {ok, fail} from "node-result";
-import type {TResultAsync} from 'node-result';
+import * as http from "http";
+import * as https from "https";
+import { URL } from "url";
+import { ok, fail } from "node-result";
+import type { IncomingHttpHeaders } from "http";
+import type { TResultAsync } from "node-result";
 
 export type HttpOptions = http.RequestOptions | https.RequestOptions;
 
 export type RequestOptions = {
-    url: URL;
-    options: HttpOptions;
-    body?: unknown;
+  url: URL;
+  options: HttpOptions;
+  body?: unknown;
 };
 
 export type RequestResponse = {
-    status: number;
-    headers: IncomingHttpHeaders,
-    body?: Buffer;
-}
+  status: number;
+  headers: IncomingHttpHeaders;
+  body?: Buffer;
+};
 
-export function request(requestOptions: RequestOptions): TResultAsync<RequestResponse, Error> {
-    const {url, options, body} = requestOptions;
-    const request = url.protocol === 'http:' ? http.request : https.request;
-    return new Promise(resolve => {
-        try {
-            const req = request(url, options, (res) => {
-                const status = res.statusCode!;
-                const headers = res.headers;
-                const contentType = headers['content-type'];
-                if (contentType) {
-                    let body = Buffer.alloc(0);
-                    res.on('data', chunk => body = Buffer.concat([body, chunk], Buffer.byteLength(body) + Buffer.byteLength(chunk)));
-                    res.on('end', () => resolve(ok({status, headers, body})));
-                } else {
-                    resolve(ok({status, headers}));
-                }
-            });
-            req.on('error', error => resolve(fail(error)));
-            if (body) req.write(body);
-            req.end();
-        } catch (error) {
-            resolve(fail(error));
+export function request(
+  requestOptions: RequestOptions
+): TResultAsync<RequestResponse, Error> {
+  const { url, options, body } = requestOptions;
+  const request = url.protocol === "http:" ? http.request : https.request;
+  return new Promise((resolve) => {
+    try {
+      const req = request(url, options, (res) => {
+        const status = res.statusCode!;
+        const headers = res.headers;
+        const contentType = headers["content-type"];
+        if (contentType) {
+          let body = Buffer.alloc(0);
+          res.on(
+            "data",
+            (chunk) =>
+              (body = Buffer.concat(
+                [body, chunk],
+                Buffer.byteLength(body) + Buffer.byteLength(chunk)
+              ))
+          );
+          res.on("end", () => resolve(ok({ status, headers, body })));
+        } else {
+          resolve(ok({ status, headers }));
         }
-    });
+      });
+      req.on("error", (error) => resolve(fail(error)));
+      if (body) req.write(body);
+      req.end();
+    } catch (error) {
+      resolve(fail(error));
+    }
+  });
 }
